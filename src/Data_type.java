dd2e502266581728c73e324e665e2c78d71a67d2
darrime <darrime627@gmail.com>
2021-02-22 16:27:52 +0900

    bug 수정

--- src/Data_type.java
+++ src/Data_type.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 
 public class Data_type {
     public static void main(String[] args) {
+        System.out.println("원하는 문자: ");
+        Scanner sc1 = new Scanner(System.in);
+        String strWant = sc1.next();
+
+
         System.out.println("반복할 횟수: ");
         Scanner sc = new Scanner(System.in);
         int want = sc.nextInt();
 
-        System.out.println("원하는 문자: ");
-        Scanner sc1 = new Scanner(System.in);
-        String strWant = sc1.next();
+
 
 
         System.out.println("-------\n" + "결과\n" + "-------");
